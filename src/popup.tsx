7a4ef56c68a1d4e8f7dd20ff5357fd587d5958e2
待看云开 <xyk953651094@qq.com>
2023-07-19 17:21:59 +0800

    修复 Firefox 图片错位问题

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import PopupComponent from "./components/popupComponent"
 
 const popupRoot = ReactDOM.createRoot(
