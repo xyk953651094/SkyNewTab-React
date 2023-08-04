fad683472fea6fbbd7d032a2effc6d44b368fd10
待看云开 <xyk953651094@qq.com>
2023-08-04 21:48:50 +0800

    popup 图片加载新增 blurhash 效果

--- src/popupComponents/popupImageComponent.tsx
+++ src/popupComponents/popupImageComponent.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import {Button, Image, message, Space} from "antd";
 import {EnvironmentOutlined, InfoCircleOutlined, UserOutlined} from "@ant-design/icons";
-import "../stylesheets/popupComponent.scss"
+import "../stylesheets/popupComponent.scss";
+import {decode} from "blurhash"
 import {getFontColor, isEmptyString} from "../typescripts/publicFunctions";
 
 const btnMaxSize = 35;
@@ -20,6 +21,7 @@ type stateType = {
     imageDescription: string,
     hoverColor: string,
     fontColor: string,
+    blurHashCode: string
 }
 
 interface PopupImageComponent {
@@ -38,7 +40,8 @@ class PopupImageComponent extends React.Component {
             imageLocation: "暂无信息",
             imageDescription: "暂无信息",
             hoverColor: "#000000",
-            fontColor: "#000000"
+            fontColor: "#000000",
+            blurHashCode: ""
         }
     }
 
@@ -77,8 +80,23 @@ class PopupImageComponent extends React.Component {
                 imagePreviewUrl: nextProps.imageData.urls.thumb,
                 imageLocation: isEmptyString(nextProps.imageData.location.name) ? "暂无信息" : nextProps.imageData.location.name,
                 imageDescription: isEmptyString(nextProps.imageData.alt_description) ? "暂无信息" : nextProps.imageData.alt_description,
-                hoverColor: nextProps.imageData.color
-            })
+                hoverColor: nextProps.imageData.color,
+                blurHashCode: nextProps.imageData.blur_hash
+            },() => {
+                if (!isEmptyString(this.state.blurHashCode)) {
+                    const popupCanvas = document.getElementById("popupCanvas") as HTMLCanvasElement | null;
+                    if (popupCanvas instanceof HTMLCanvasElement) {
+                        let blurHashImage = decode(this.state.blurHashCode, popupCanvas.width, popupCanvas.height);
+                        let ctx = popupCanvas.getContext("2d");
+                        if (ctx) {
+                            const imageData = new ImageData(blurHashImage, popupCanvas.width, popupCanvas.height);
+                            ctx.putImageData(imageData, 0, 0);
+                        }
+
+                        popupCanvas.className = "popupCanvas wallpaperFadeIn";
+                    }
+                }}
+            )
         }
 
         if (nextProps.fontColor !== prevProps.fontColor) {
@@ -93,10 +111,14 @@ class PopupImageComponent extends React.Component {
             <Space>
                 <Image
                     width={200}
+                    height={120}
                     preview={false}
                     alt={"暂无图片"}
                     src={this.state.imagePreviewUrl}
                     style={{borderRadius: "10px"}}
+                    placeholder={
+                        <canvas id={"popupCanvas"} className={"popupCanvas"}></canvas>
+                    }
                 />
                 <Space direction={"vertical"}>
                     <Button type={"text"} shape={"round"} icon={<UserOutlined/>}
