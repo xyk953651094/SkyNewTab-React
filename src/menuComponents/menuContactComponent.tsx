dd443dab0960a996a4aaf0ce514eabb84abc7b6a
待看云开 <xyk953651094@qq.com>
2024-02-25 18:36:42 +0800

    提升使用体验

--- src/menuComponents/menuContactComponent.tsx
+++ src/menuComponents/menuContactComponent.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Button, Card, Col, Row} from "antd";
-import {DislikeOutlined, GithubOutlined, GitlabOutlined, LikeOutlined, MailOutlined} from "@ant-design/icons";
+import {DislikeOutlined, GithubOutlined, GitlabOutlined, LikeOutlined, MailOutlined, BulbOutlined} from "@ant-design/icons";
 import {btnMouseOut, btnMouseOver} from "../typescripts/publicFunctions";
 import {PreferenceDataInterface} from "../typescripts/publicInterface";
 
@@ -38,6 +38,24 @@ class MenuContactComponent extends React.Component {
             >
                 <Row gutter={[0, 8]}>
                     <Col span="12">
+                        <Button type={"text"} shape={this.props.preferenceData.buttonShape} icon={<GithubOutlined/>}
+                                href={"https://github.com/xyk953651094/"} target={"_self"}
+                                onMouseOver={btnMouseOver.bind(this, this.props.hoverColor)}
+                                onMouseOut={btnMouseOut.bind(this, this.props.fontColor)}
+                                style={{color: this.props.fontColor}}>
+                            {"作者主页"}
+                        </Button>
+                    </Col>
+                    <Col span="12">
+                        <Button type={"text"} shape={this.props.preferenceData.buttonShape} icon={<GitlabOutlined/>}
+                                href={"https://gitlab.com/xyk953651094/"} target={"_self"}
+                                onMouseOver={btnMouseOver.bind(this, this.props.hoverColor)}
+                                onMouseOut={btnMouseOut.bind(this, this.props.fontColor)}
+                                style={{color: this.props.fontColor}}>
+                            {"作者主页"}
+                        </Button>
+                    </Col>
+                    <Col span="12">
                         <Button type={"text"} shape={this.props.preferenceData.buttonShape} icon={<LikeOutlined/>}
                                 href={"mailto:xyk953651094@qq.com?&subject=云开新标签页-功能建议"} target={"_self"}
                                 onMouseOver={btnMouseOver.bind(this, this.props.hoverColor)}
@@ -56,21 +74,11 @@ class MenuContactComponent extends React.Component {
                         </Button>
                     </Col>
                     <Col span="12">
-                        <Button type={"text"} shape={this.props.preferenceData.buttonShape} icon={<GithubOutlined/>}
-                                href={"https://github.com/xyk953651094/"} target={"_self"}
+                        <Button type={"text"} shape={this.props.preferenceData.buttonShape} icon={<BulbOutlined />}
                                 onMouseOver={btnMouseOver.bind(this, this.props.hoverColor)}
                                 onMouseOut={btnMouseOut.bind(this, this.props.fontColor)}
                                 style={{color: this.props.fontColor}}>
-                            {"作者主页"}
-                        </Button>
-                    </Col>
-                    <Col span="12">
-                        <Button type={"text"} shape={this.props.preferenceData.buttonShape} icon={<GitlabOutlined/>}
-                                href={"https://gitlab.com/xyk953651094/"} target={"_self"}
-                                onMouseOver={btnMouseOver.bind(this, this.props.hoverColor)}
-                                onMouseOut={btnMouseOut.bind(this, this.props.fontColor)}
-                                style={{color: this.props.fontColor}}>
-                            {"作者主页"}
+                            {"联系作者前请先查阅帮助文档"}
                         </Button>
                     </Col>
                 </Row>
