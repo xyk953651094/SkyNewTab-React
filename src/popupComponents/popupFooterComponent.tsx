aa0dbbfb083e90048400325d20413a731efa1e27
待看云开 <xyk953651094@qq.com>
2024-02-26 16:08:30 +0800

    提升使用体验

--- src/popupComponents/popupFooterComponent.tsx
+++ src/popupComponents/popupFooterComponent.tsx
@@ -31,14 +31,14 @@ class PopupFooterComponent extends React.Component {
                         onMouseOver={btnMouseOver.bind(this, this.props.hoverColor)}
                         onMouseOut={btnMouseOut.bind(this, this.props.fontColor)}
                         style={{color: this.props.fontColor}}>
-                    GitHub 产品主页
+                    产品主页
                 </Button>
                 <Button type={"text"} shape={this.props.preferenceData.buttonShape} icon={<GitlabOutlined/>}
                         href={"https://gitlab.com/xyk953651094/SkyNewTab-React/"} target={"_blank"}
                         onMouseOver={btnMouseOver.bind(this, this.props.hoverColor)}
                         onMouseOut={btnMouseOut.bind(this, this.props.fontColor)}
                         style={{color: this.props.fontColor}}>
-                    GitLab 产品主页
+                    产品主页
                 </Button>
                 <Button type={"text"} shape={this.props.preferenceData.buttonShape} icon={<NotificationOutlined/>}
                         href={"https://xyk953651094.blogspot.com/"} target={"_blank"}
